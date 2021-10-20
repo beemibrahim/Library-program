6cec261bee0d1070289b9ae8f2c5efdee2bf996b
ibrahim muhmud usmani <92852749+beemibrahim@users.noreply.github.com>
2021-10-20 16:56:32 +0500

    Add files via upload

---
+++ Library.hpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+class book {
+public:
+  int id = 0;
+  string name;
+  string author;
+  int pages = 0;
+};
+
+class Command {
+public:
+  bool worked = false;
+  // find book = 1
+  // create = 2
+  // find_everything = 3
+  // update book = 4
+  // delete everything = 5
+  // delete book = 6
+  int command_type = 0;
+  string command;
+  bool id_gon;
+  bool name_gon;
+  bool author_gon;
+  bool page_gon;
+  int id = 0;
+  string name;
+  string author;
+  int pages = 0;
+  // Only for update
+  bool name2;
+  bool author2;
+  bool pages2;
+};
+
+class UserInput {
+public:
+  Command GetInput(vector<book> books);
+
+private:
+  string SpaceRemover_for_update(string once);
+  int last_id = 0;
+  void Remove_All_Spaces();
+  string input_store;
+};
+
+class Library {
+public:
+  void library_intro();
+  UserInput input;
+  Command command;
+  void create(); // need to take a library
+  void delete_();
+  void update();
+  void delete_everything();
+  void find_everything();
+  void find();
+  vector<book> books;
+};
+
+class Library_Application {
+  UserInput user_input;
+  Library library;
+
+public:
+  void PrintOutput();
+};
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include "Library.hpp"
+
+void Library_MainPart() {
+  Library library;
+  library.library_intro();
+  for (;;) {
+    library.command = library.input.GetInput(library.books);
+    if (library.command.worked == false) {
+      continue;
+    }
+    if (library.command.command_type == 1) {
+      library.find();
+      continue;
+    }
+    if (library.command.command_type == 2) {
+      library.create();
+      continue;
+    }
+    if (library.command.command_type == 3) {
+      library.find_everything();
+      continue;
+    }
+    if (library.command.command_type == 4) {
+      library.update();
+      continue;
+    }
+    if (library.command.command_type == 5) {
+      library.delete_everything();
+      continue;
+    }
+    if (library.command.command_type == 6) {
+      library.delete_();
+      continue;
+    }
+  }
+}
+
+int main() { Library_MainPart(); }
